34111dff1fc08c88e1f8bdd5f1b4709d7b54ca9d
skytales <glnjendu2004@gmail.com>
2024-03-12 17:52:17 -0400

    bruh

--- phone_number.py
+++ phone_number.py
@@ -1,11 +1,15 @@
 from flask import Flask
+import sqlite3
+con = sqlite3.connect("tutorial.db")
+cur = con.cursor()
+
 
 phone_idx = 0
 
-@app.route('/users/<name>')
+@app.route('/users/<name>', methods=['GET'])
 def phone_number(name):
-    res = cur.execute("SELECT name FROM sqlite_master")
+    res = cur.execute("SELECT phone_number FROM USERS WHERE name='"+name +"'")
     users = res.fetchone()  
     if len(users != 0):
-        print(users[phone_idx])
+        print(users)
 
